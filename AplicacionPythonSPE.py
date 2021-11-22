9273244ff7c16b27bdef146f9913ec6bb5935e23
Jose Eduardo Ubillus <joseeduardoubillus@hotmail.com>
2021-11-21 23:20:51 -0800

    Update AplicacionPythonSPE.py
    English version

--- AplicacionPythonSPE.py
+++ AplicacionPythonSPE.py
@@ -8,7 +8,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 #Configuraciones basicas de la pantalla de Streamlit
-st.set_page_config(page_title='Diseño de Trayectorias de Pozos de Hidrocarburos',
+st.set_page_config(page_title='Well Trajectory Design',
                    page_icon='♣',
                    layout='centered',
                    initial_sidebar_state='collapsed')
@@ -48,30 +48,27 @@ def mdt (md1,md2,md3):
 
 #Estructuramos la pagina a nuestro gusto
 #Principal
-st.image('perforacion.jpg',use_column_width=True,)
-st.header('HERRAMIENTA DE DISEÑO DE TRAYECTORIA DE POZOS')
-st.subheader("Society of Petroleum Engineering - Escuela Politecnica Nacional Student Chapter")
-st.text('''Las ecuaciones utilizadas para elaborar la trayectoria de un pozo pertenecen 
-al libro Well Engineering and Construction - Rabia''')
-st.subheader('Informacion de la trayectoria')
-st.text('Esta informacion dependera del tipo de pozo seleccionado')
+st.image('BANNER (AMARILLO).jpg',use_column_width=True,)
+st.header('WELL TRAJECTORY DESIGN TOOL')
+st.text('''The equations used belong to the book Well Engineering and Construction by Rabia''')
+st.subheader('Information Required')
+st.text('Depends on the type of well to be build')
 #Barra de contenido
-st.sidebar.header('Tipos de Pozos')
-tipo_pozo=st.sidebar.radio('Seleccione el tipo de pozo a diseñar',('Tipo 1', 'Tipo 2'))
+st.sidebar.header('Well types')
+tipo_pozo=st.sidebar.radio('Choose the type of well to design',('Type 1', 'Type 2'))
 st.sidebar.text("")
 st.sidebar.text("")
 st.sidebar.text("")
-st.sidebar.text("Elaborado por: Jose Eduardo Ubillus")
-st.sidebar.image("spe_logo.png")
+st.sidebar.text("Developed by: Jose Eduardo Ubillus")
 
 
-if tipo_pozo == 'Tipo 1':
-    bur=st.number_input('Ingrese el BUR')
-    v3=st.number_input('Ingrese el TVD del target')
-    v1=st.number_input('Ingrese el KOP')
-    d2=st.number_input('Ingrese la distancia horizontal del target')
-    st.subheader("Resultados del diseño de la trayectoria del pozo")
-    check_resultados=st.checkbox('Visualizar resultados de la trayectoria',value=False)
+if tipo_pozo == 'Type 1':
+    bur=st.number_input('Input BUR')
+    v3=st.number_input('Input Target TVD')
+    v1=st.number_input('Input KOP')
+    d2=st.number_input('Input the Horizontal Displacement of the Target')
+    st.subheader("Well Trajectory Results")
+    check_resultados=st.checkbox('View results',value=False)
     if check_resultados == True:
         r=radio_curvatura(bur)
         alfa=alfa_max(r,d2,v3,v1)
@@ -81,36 +78,36 @@ if tipo_pozo == 'Tipo 1':
         MD3=md3(v3,V2,alfa)
         MDT=mdt(v1,MD2,MD3)
 
-        st.text(f'El radio de curvatura es: {r} ft')
-        st.text(f'El angulo maximo de inclinacion es: {alfa} grados')
-        st.text(f'El MD en seccion de construccion es: {MD2} ft')
-        st.text(f'La distancia vertical en seccion de construccion es: {V2} ft')
-        st.text(f'La distancia horizontal al final de la seccion de construccion es: {D1} ft')
-        st.text(f'El MD en seccion tangente es: {MD3} ft')
-        st.text(f'El MD total es: {MDT} ft')
+        st.text(f'The curvature radius is: {r} ft')
+        st.text(f'The max inclination angle: {alfa} grados')
+        st.text(f'The MD in the build up section is: {MD2} ft')
+        st.text(f'The vertical section in the build up is: {V2} ft')
+        st.text(f'The Horizontal Displacement at the end of the build up section is: {D1} ft')
+        st.text(f'The MD in the tangential section is: {MD3} ft')
+        st.text(f'The total MD is: {MDT} ft')
 
         #tabla de datos
-        columnas=('Coordenada X', 'Coordenada Y')
+        columnas=('X', 'Y')
         data=[(0,0),(0,v1),(D1,V2),(d2,v3)]
         tabla_coordenadas=pd.DataFrame(data=data,columns=columnas)
         col1, col2= st.beta_columns([1.5,1])
         with col1:
-            st.subheader('Coordenadas necesarias en pozo TIPO I')
+            st.subheader('Coordenates needed for the Type 1 Well')
         with col2:
             st.dataframe(tabla_coordenadas)
         
         #grafico de la trayectoria
         #Configuracion de las caracteristicas de la figura
-        st.subheader('Grafica de la trayectoria del pozo TIPO I')
+        st.subheader('Type 1 Well Graph')
         fig, ax1= plt.subplots()
-        ax1.set_title('TRAYECTORIA POZO TIPO 1')
+        ax1.set_title('Trajectory')
         ax1.set_ylabel('TVD (ft)')
         ax1.set_xlim(xmin=-100,xmax=v3+1000)
-        ax1.set_xlabel('Distancia horizontal (ft)')
+        ax1.set_xlabel('Horizontal Displacement (ft)')
         ax1.set_ylim(ymin=0,ymax=v3+1000)
         ax1.invert_yaxis()
         #Primera seccion
-        ax1.plot((tabla_coordenadas['Coordenada X'][0],tabla_coordenadas['Coordenada X'][1]),(tabla_coordenadas['Coordenada Y'][0],tabla_coordenadas['Coordenada Y'][1]),label='KOP')
+        ax1.plot((tabla_coordenadas['X'][0],tabla_coordenadas['X'][1]),(tabla_coordenadas['Y'][0],tabla_coordenadas['Y'][1]),label='KOP')
         #Seccion de construccion
         def arco (r,alfa):
             x=[]
@@ -122,16 +119,16 @@ if tipo_pozo == 'Tipo 1':
             return x,y
         
         x,y=arco(r,alfa)
-        ax1.plot(x,y,label='Seccion de Construccion')
+        ax1.plot(x,y,label='Build Up Section')
         #Seccion tangencial
-        ax1.plot((tabla_coordenadas['Coordenada X'][2],tabla_coordenadas['Coordenada X'][3]),(tabla_coordenadas['Coordenada Y'][2],tabla_coordenadas['Coordenada Y'][3]),label='Seccion tangencial')
+        ax1.plot((tabla_coordenadas['X'][2],tabla_coordenadas['X'][3]),(tabla_coordenadas['Y'][2],tabla_coordenadas['Y'][3]),label='Tangential Section')
         #Locacion del target
         ax1.plot(d2,v3,'bo',label='Target')
         #Labels
         ax1.legend()
         st.pyplot(fig)
-if tipo_pozo == 'Tipo 2':
-    st.subheader('En construccion...')
+if tipo_pozo == 'Type 2':
+    st.subheader('Under Construction...')
 
     
     
